46f337173c6c72fee50e8fef6a617c7c1182c0f6
Chaddy1748 <160126933+Chaddy1748@users.noreply.github.com>
2024-03-02 14:51:16 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [CHADWICK HORACE 
-  [`+254782775836', 'CHADWICK' s  BOT', true],
+  [`+254795297855', 'CHADWICK' s  BOT', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
